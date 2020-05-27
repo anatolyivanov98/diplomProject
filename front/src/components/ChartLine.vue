4659bc5fcc3290e764a77267e7ebae7f68069375
letsleroyme <letsleroyme@gmail.com>
2020-05-27 18:42:57 +0700

    Revert "ProcFile"
    This reverts commit c6b9a8e2aa653cf9c955e5a047e4077ed764cd71.

---
+++ front/src/components/ChartLine.vue
@@ -0,0 +1,36 @@
+<div class="col l12">
+  <div class="table-chart">
+    <canvas id="canvas"></canvas>
+  </div>
+</div>
+
+<script>
+  import {Line} from 'vue-chartjs';
+  export default {
+    name: "ChartLine",
+    data: () =>({
+      chartData: ''
+    }),
+    extends: Line,
+    mounted() {
+      this.chartData = this.$store.state.table.chartData
+      this.renderChart({
+        labels: this.chartData.labels[1],
+        datasets: [{
+          label: this.chartData.header[1],
+          data: this.chartData.data[1],
+
+          borderColor: [
+            '#015668',
+
+          ],
+          borderWidth: 1
+        }]
+      })
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ front/src/components/ChartStackedBar.vue
@@ -0,0 +1,79 @@
+<div class="col l12">
+  <div class="table-chart">
+    <canvas id="canvas"></canvas>
+  </div>
+</div>
+
+<script>
+  import {Bar} from 'vue-chartjs';
+  export default {
+    name: "ChartStackedBar",
+    data: () =>({
+      chartData: ''
+    }),
+    extends: Bar,
+    mounted() {
+      this.chartData = this.$store.state.table.chartData
+
+      let counter = 0
+      for( let n in this.chartData.header) {
+        counter++
+      }
+       const Colors = [
+        '#293a80',
+        '#1eb2a6',
+        '#537ec5',
+        '#142850',
+        '#00a8cc',
+        '#60a9a6',
+        '#ffa34d',
+        '#ffd31d',
+        '#574f7d',
+
+        '#bb7171',
+        '#c72c41',
+        '#7189bf',
+        '#574f7d',
+        '#574f7d',
+        '#574f7d',
+        '#574f7d',
+        '#574f7d',
+        '#574f7d',
+        '#574f7d'
+      ]
+
+      let dd = []
+      for( let i = 0; i < counter; i++){
+            dd[i] =  {label: this.chartData.header[i], data: this.chartData.data[i], backgroundColor:Colors[i] }
+      }
+
+      const chartOptions = {
+          scales: {
+            xAxes: [{
+              stacked: true
+            }],
+            yAxes: [{
+            stacked: true
+            }]
+          }
+        }
+
+      const allData = {
+          labels: this.chartData.labels,
+          datasets: dd
+        }
+
+      this.renderChart(allData, chartOptions)
+   }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+
+
+
+
